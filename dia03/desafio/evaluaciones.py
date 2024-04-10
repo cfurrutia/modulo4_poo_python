e83a8248231f70c929b15cd6a616483ca2ecde8e
cfurrutia <ing.cfurrutia@gmail.com>
2024-04-10 13:55:37 -0400

    Desafío - Métodos y atributos -  Revision

--- dia03/desafio/evaluaciones.py
+++ dia03/desafio/evaluaciones.py
@@ -8,19 +8,17 @@ print("Ingredientes proteicos posibles:", Pizza.ingredientes_proteicos)
 print("Ingredientes vegetales posibles:", Pizza.ingredientes_vegetales)
 print("Tipos de masa posibles:", Pizza.tipos_masa)
 
-# validacion de entrada de salsa de tomate
-print("¿Salsa de tomate está presente en la lista? \n", Pizza.validar_elemento("salsa de tomate", ["salsa de tomate", "salsa bbq"]))
-
 # Crear una instancia de la clase Pizza
 pizza = Pizza()
+
+# Realizar un pedido
 pizza.realizar_pedido()
 
 # Mostrar los ingredientes y tipo de masa del pedido
-print("\n Ingredientes vegetales:", pizza.ingrediente_vegetal1, ",", pizza.ingrediente_vegetal2)
+print("Ingredientes vegetales:", pizza.primer_ingrediente_vegetal, pizza.segundo_ingrediente_vegetal)
 print("Ingrediente proteico:", pizza.ingrediente_proteico)
 print("Tipo de masa:", pizza.tipo_masa)
-print("¿Es una pizza válida? \n","si" if pizza.es_valida else "no")
-#mostrar en pantalla si la clase Pizza es una pizza válida o no  
-print("¿Es la clase Pizza una pizza válida? \n", "si" if Pizza().es_valida else "no")
 
-
+# Verificar si el pedido es válido
+print("¿Es una pizza válida? \n","si" if pizza.es_pizza_valida else "no")
+print("¿Es la clase Pizza una pizza válida? \n", "si" if hasattr(Pizza, 'si') else "no")
--- dia03/desafio/pizza.py
+++ dia03/desafio/pizza.py
@@ -1,40 +1,27 @@
+# Clase Pizza
 class Pizza:
-# Atributos de clase - ingredientes
+    # Atributos de clase
     ingredientes_proteicos = ["pollo", "vacuno", "carne vegetal"]
     ingredientes_vegetales = ["tomate", "aceitunas", "champiñones"]
     tipos_masa = ["tradicional", "delgada"]
 
+    # Método para validar un elemento en una lista de casos posibles
     @staticmethod
     def validar_elemento(elemento, valores_posibles):
-        """Método para validar un elemento en una lista de casos posibles
-        Args:
-            elemento (_type_): Elemento a validar
-            valores_posibles (lista): Lista de valores posibles
-        Returns:
-            bool : True si el elemento es válido, False en caso contrario
-        """
-        return elemento in valores_posibles
-    
+        if elemento in valores_posibles:
+            return True
+        else:
+            return False
+
+    # Método para realizar un pedido
     def realizar_pedido(self):
-        """Método para realizar un pedido
-        """
         self.ingrediente_proteico = input("Ingrese el ingrediente proteico: ")
-        self.ingrediente_vegetal1 = input("Ingrese el primer ingrediente vegetal: ")
-        self.ingrediente_vegetal2 = input("Ingrese el segundo ingrediente vegetal: ")
-        self.tipo_masa = input("Ingrese el tipo de masa : ")
-            
-        #  Verificar si el pedido es válido
-        self.es_valida = self.validar_elemento(self.ingrediente_proteico, self.ingredientes_proteicos) and \
-        self.validar_elemento(self.ingrediente_vegetal1, self.ingredientes_vegetales) and \
-        self.validar_elemento(self.ingrediente_vegetal2, self.ingredientes_vegetales) and \
-        self.validar_elemento(self.tipo_masa, self.tipos_masa)
-        
+        self.primer_ingrediente_vegetal = input("Ingrese el primer ingrediente vegetal: ")
+        self.segundo_ingrediente_vegetal = input("Ingrese el segundo ingrediente vegetal: ")
+        self.tipo_masa = input("Ingrese el tipo de masa: ")
+
     
-# prueba de pedidos   ( __init__, metodo especial,constructor de una clase)
-    def __init__(self):
-        self.ingrediente_proteico = None
-        self.ingrediente_vegetal1 = None
-        self.ingrediente_vegetal2 = None
-        self.tipo_masa = None
-        self.es_valida = False         
-   
+        self.es_pizza_valida = self.validar_elemento(self.ingrediente_proteico, self.ingredientes_proteicos) and \
+        self.validar_elemento(self.primer_ingrediente_vegetal, self.ingredientes_vegetales) and \
+        self.validar_elemento(self.segundo_ingrediente_vegetal, self.ingredientes_vegetales) and \
+        self.validar_elemento(self.tipo_masa, self.tipos_masa)
