63c2a992d2358fcd564b417dbd4392d9b3b1a5dc
cfurrutia <ing.cfurrutia@gmail.com>
2024-04-18 13:13:30 -0400

    Desafío evaluado - Diagrama de clases

---
+++ dia09/Model/listado_respuestas.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class ListadoRespuestas:
+    def __init__(self, usuario:str):
+        self.__lista_respuestas = []
+        self.__usuario = usuario
+
---
+++ dia09/Model/usuario.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Usuario:
+    def __init__(self):
+        self.correo = None
+        self.edad = None
+        self.region = None
+
+    def contesta_encuesta(self, encuesta):
+        pass
+
