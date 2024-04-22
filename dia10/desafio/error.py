5280b88d0a73a3e8ef242662485d608e21017f4e
cfurrutia <ing.cfurrutia@gmail.com>
2024-04-22 09:33:53 -0400

    revision del grupo

--- dia10/desafio/error.py
+++ dia10/desafio/error.py
@@ -1,21 +1,21 @@
 class DimensionError(Exception):
-    def __init__(self, mensaje:str, dimension=None, maximo=None):
+    def __init__(self, mensaje: str, dimension=None, maximo=None):
         self.mensaje = mensaje
         self.dimension = dimension
         self.maximo = maximo
-    
-    def __str__(self): #sobrecarga del método __str__ para generar mensajes de error
-        if self.dimension is not None and self.maximo is not None: #error cuando están todos los datos.
+
+    def __str__(self):
+        # Sobrecarga del método __str__ para generar mensajes de error
+        if self.dimension is not None and self.maximo is not None:
+            # Error cuando están todos los datos.
             return f"{self.mensaje} La dimensión ingresada es: {self.dimension} y el máximo es: {self.maximo}."
         else:
-            return super().__str__() #en caso de que no se cumplan esas condiciones y se llame al error, llamado al método padre con super, 
-        #solo retorna el "mensaje"
-            
-    
+            return super().__str__()  # En caso de que no se cumplan esas condiciones y se llame al error, llamado al método padre con super, solo retorna el "mensaje"
+
 if __name__ == "__main__":
-    foto1 = DimensionError("ERROR Dimensión Inválida") #ERROR Dimensión Inválida
-    foto2 = DimensionError("ERROR Dimensión Inválida",2800, 2500) #ERROR Dimensión Inválida La dimensión ingresada es: 2800 y el máximo es: 2500.
-    foto3 = DimensionError("ERROR Dimensión Inválida",-50,2500) #ERROR Dimensión Inválida La dimensión ingresada es: -50 y el máximo es: 2500.
+    foto1 = DimensionError("ERROR Dimensión Inválida")  # ERROR Dimensión Inválida
+    foto2 = DimensionError("ERROR Dimensión Inválida", 2800, 2500)  # ERROR Dimensión Inválida La dimensión ingresada es: 2800 y el máximo es: 2500.
+    foto3 = DimensionError("ERROR Dimensión Inválida", -50, 2500)  # ERROR Dimensión Inválida La dimensión ingresada es: -50 y el máximo es: 2500.
     print(foto1)
     print(foto2)
     print(foto3)
--- dia10/desafio/foto.py
+++ dia10/desafio/foto.py
@@ -9,45 +9,44 @@ Diego Larenas
 
 from error import DimensionError
 
-class Foto():
+class Foto:
     MAX = 2500
 
     def __init__(self, ancho: int, alto: int, ruta: str):
         self.__ancho = ancho
         self.__alto = alto
-        ruta = ruta
+        self.ruta = ruta  # Asignación correcta del atributo ruta
 
     @property
-    def ancho(self) -> int: #getter
+    def ancho(self) -> int:
         return self.__ancho
+
     @ancho.setter
-    def ancho(self, ancho:int): #setter con control de errores
+    def ancho(self, ancho: int):
         try:
-            if ancho > 0 and ancho < self.MAX: #caso sin error
-                self.__ancho = ancho #setter funciona
-            elif ancho < 1: #error por dato menor a 1
+            if 0 < ancho < self.MAX: 
+                self.__ancho = ancho
+            elif ancho < 1:  # Error por dato menor a 1
                 raise DimensionError("El valor del ancho debe ser mayor que 0.", ancho, self.MAX)
-            elif ancho > self.MAX: #error por dato mayor que el máximo
+            elif ancho > self.MAX:  # Error por dato mayor que el máximo
                 raise DimensionError("El valor del ancho excede el máximo.", ancho, self.MAX)
-        except DimensionError as dError: #impresión del error en caso de un Dimension Error
+        except DimensionError as dError:
             print(dError)
-        except TypeError: #En caso de errores de tipo
+        except TypeError:
             print("Error en el ingreso del ancho")
         else:
-            print("Ancho editado") #si no hay errores, que reporte el cambio realizado
-
-        
+            print("Ancho editado")
 
     @property
     def alto(self) -> int:
         return self.__alto
 
     @alto.setter
-    def alto(self, alto) -> None:
+    def alto(self, alto):
         try:
-            if alto > 0 and alto < self.MAX:
+            if 0 < alto < self.MAX:
                 self.__alto = alto
-            elif alto < 1 :
+            elif alto < 1:
                 raise DimensionError("El valor del alto debe ser mayor que 0.", alto, self.MAX)
             elif alto > self.MAX:
                 raise DimensionError("El valor del alto excede el máximo.", alto, self.MAX)
@@ -58,13 +57,12 @@ class Foto():
         else:
             print("Alto editado")
 
-
 if __name__ == "__main__":
     foto = Foto(1500, 2100, "blabla")
-    foto.ancho = 0 #El valor del ancho debe ser mayor que 0. La dimensión ingresada es: 0 y el máximo es: 2500.
-    foto.ancho = 3000 #El valor del ancho excede el máximo. La dimensión ingresada es: 3000 y el máximo es: 2500.
-    foto.alto = 0 #El valor del alto debe ser mayor que 0. La dimensión ingresada es: 0 y el máximo es: 2500.
-    foto.alto = 3000 #El valor del alto excede el máximo. La dimensión ingresada es: 3000 y el máximo es: 2500.
-    foto.alto = "a" #Error en el ingreso del alto
-    foto.ancho = 2480 #Ancho editado
-    foto.alto = 2495 #Alto editado
+    foto.ancho = 0  # El valor del ancho debe ser mayor que 0. La dimensión ingresada es: 0 y el máximo es: 2500.
+    foto.ancho = 3000  # El valor del ancho excede el máximo. La dimensión ingresada es: 3000 y el máximo es: 2500.
+    foto.alto = 0  # El valor del alto debe ser mayor que 0. La dimensión ingresada es: 0 y el máximo es: 2500.
+    foto.alto = 3000  # El valor del alto excede el máximo. La dimensión ingresada es: 3000 y el máximo es: 2500.
+    foto.alto = "a"  # Error en el ingreso del alto
+    foto.ancho = 2480  # Ancho editado
+    foto.alto = 2495  # Alto editado
