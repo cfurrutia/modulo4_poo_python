0895fa3142c6537023a5366edb9595eb0cc686b6
cfurrutia <ing.cfurrutia@gmail.com>
2024-04-23 11:23:28 -0400

    Desafío evaluado - Instancias de usuario

--- dia12/script.py
+++ dia12/script.py
@@ -10,11 +10,15 @@ try:
                 #se crea una instancia de Usuario con los parámetros desde datos_linea y se almacenan en lista_usuarios
                 lista_usuarios.append(Usuario(datos_linea.get("nombre"), datos_linea.get("apellido"), datos_linea.get("email"), datos_linea.get("genero")))
             except Exception as e: #en caso de haber un error se le da un alias
-                with open(f"dia12/logs/error.log", "a+",encoding="utf-8") as log: #y se escribe en error.log cuál fue el error
-                    log.write(f"Error al procesar la línea: {linea}\tExcepción: {e}\n")
+                with open(f"dia12/logs/error_usuario.log", "a+",encoding="utf-8") as usuariolog: #y se escribe en error.log cuál fue el error
+                    usuariolog.write(f"Error al procesar la línea: {linea}\tExcepción: {e}\n")
                 print("Ha habido un error:", e ) #también se imprime dicho error por consola
             else:
                 print(f"Usuario creado: {datos_linea.get("nombre")} {datos_linea.get("apellido")}") #si no hay errores, imprime el usuario creado con su nombre y apellido
+            finally:
+                usuarios.close()
+                usuariolog.close()
 except Exception as error: #prueba, en caso de no estar el archivo lanza este error
-    with open(f"dia12/logs/error.log", "a+",encoding="utf-8") as log:
-        log.write(f"Error al abrir el archivo.\n\tExcepción: {error}\n")
+    with open(f"dia12/logs/error_archivo.log", "a+",encoding="utf-8") as archivolog:
+        archivolog.write(f"Error al abrir el archivo.\n\tExcepción: {error}\n")
+    archivolog.close()
