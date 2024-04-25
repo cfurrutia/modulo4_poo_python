ad88e8e287a9f88373984ecd98cd00dd05923902
cfurrutia <ing.cfurrutia@gmail.com>
2024-04-25 15:23:40 -0400

    prueba desafio 4

--- dia13/prueba_modulo4/anuncio.py
+++ dia13/prueba_modulo4/anuncio.py
@@ -5,12 +5,13 @@ class Anuncio(ABC):
     FORMATOS = ()
     SUB_TIPOS = ()
 
-    def __init__(self, sub_tipo):
+    def __init__(self, alto: int, ancho: int, url_archivo: str, url_clic: str, sub_tipo: str):
         self.__sub_tipo = sub_tipo
-        self.__alto = 1
-        self.__ancho = 1
-        self.__url_clic = ""
-        self.__url_archivo = ""
+        self.__alto = alto
+        self.__ancho = ancho
+        self.__url_clic = url_clic
+        self.__url_archivo = url_archivo
+        self.__sub_tipo = sub_tipo
 
     @property
     def sub_tipo(self):
@@ -34,18 +35,18 @@ class Anuncio(ABC):
     @property
     def url_clic(self):
         return self.__url_clic
-
+    
     @url_clic.setter
-    def url_clic(self, nueva_url_clic):
-        self.__url_clic = nueva_url_clic
+    def url_clic(self, url_clic):
+        self.__url_clic = url_clic
 
     @property
     def url_archivo(self):
         return self.__url_archivo
-
+    
     @url_archivo.setter
-    def url_archivo(self, nueva_url_archivo):
-        self.__url_archivo = nueva_url_archivo
+    def url_archivo(self, url_archivo):
+        self.__url_archivo = url_archivo
 
     @abstractmethod
     def comprimir_anuncio(self):
@@ -59,17 +60,17 @@ class Anuncio(ABC):
     @staticmethod
     def mostrar_formatos(FORMATOS, SUB_TIPOS):
         print(f"{FORMATOS}:")
-        print("==========")
+        print("===========")
         for subtipo in SUB_TIPOS:
             print(f"- {subtipo}")
             
 
 class Video(Anuncio):
-    FORMATOS = ('Video')
-    SUB_TIPOS = ('instream', 'outstream')
+    FORMATOS = ("Video")
+    SUB_TIPOS = ("instream", "outstream")
 
     def __init__(self, sub_tipo, duracion):
-        super().__init__(sub_tipo)
+        super().__init__(1, 1, '', '', sub_tipo) 
         self.__duracion = duracion
 
     @property
@@ -95,8 +96,8 @@ class Video(Anuncio):
         print("RECORTE DE VIDEO NO IMPLEMENTADO AÚN")
 
 class Display(Anuncio):
-    FORMATOS = ('Display')
-    SUB_TIPOS = ('tradicional', 'nativo')
+    FORMATOS = ("Display")
+    SUB_TIPOS = ("tradicional", "nativo")
 
     def comprimir_anuncio(self):
         print("COMPRESIÓN DE ANUNCIOS DISPLAY NO IMPLEMENTADA AÚN")
@@ -105,8 +106,8 @@ class Display(Anuncio):
         print("REDIMENSIONAMIENTO DE ANUNCIOS DISPLAY NO IMPLEMENTADO AÚN")
 
 class Social(Anuncio):
-    FORMATOS = ('Social')
-    SUB_TIPOS = ('facebook', 'linkedin')
+    FORMATOS = ("Social")
+    SUB_TIPOS = ("facebook", "linkedin")
 
     def comprimir_anuncio(self):
         print("COMPRESIÓN DE ANUNCIOS DE REDES SOCIALES NO IMPLEMENTADA AÚN")
